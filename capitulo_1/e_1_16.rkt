e9cb172f6bd59574ccca364382ed413e6219cbd2
DanielVillalon <dvg136@alumnos.unican.es>
2025-09-23 18:23:36 +0200

    Ejercicio 1.16

---
+++ capitulo_1/e_1_16.rkt
@@ -0,0 +1,14 @@
+#lang racket
+(define (fast-expt a b n)
+  (cond ((= n 0)
+         a)
+        ((even? n)
+         (fast-expt a (square b) (/ n 2)))
+        (else
+         (fast-expt (* a b) b (- n 1)))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (square n)
+  (* n n))
